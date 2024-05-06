f4c91eeed9c36e65b2f56cc37a1d72c1e2561499
zikang <1876079081@qq.com>
2024-05-06 14:23:58 +0800

    添加注释

--- src/main/java/com/example/mapperquickgenerationtool/mapper/UserMapper.java
+++
@@ -1,9 +0,0 @@
-package com.example.mapperquickgenerationtool.mapper;
-
-import com.example.mapperquickgenerationtool.model.User;
-
-public interface UserMapper {
-    int insert(User row);
-
-    int insertSelective(User row);
-}
